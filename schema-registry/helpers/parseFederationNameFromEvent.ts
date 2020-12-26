f1c41af97b931821e62830ae72b2187c628fbb39
Thomas <thomasjamescrawford96@gmail.com>
2020-12-26 23:13:22 +0800

    Test ESLint action

--- schema-registry/helpers/parseFederationNameFromEvent.ts
+++ schema-registry/helpers/parseFederationNameFromEvent.ts
@@ -4,7 +4,7 @@ export const parseFederationNameFromEvent = (event: APIGatewayProxyEvent) => {
   const federationName = event.pathParameters?.id;
 
   if (!federationName) {
-    throw new Error('Federation name not supplied in URL');
+    throw new Error("Federation name not supplied in URL");
   }
 
   return federationName;
